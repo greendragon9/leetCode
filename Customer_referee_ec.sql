255b11a19f989648dd0d453bbea80083cab02eee
greendragon9 <gopikasholmes@gmail.com>
2022-02-21 23:15:08 -0500

    2/21 Commits

---
+++ Customer_referee_ec.sql
@@ -0,0 +1,22 @@
+/*
+584. Find Customer Referee
+https://leetcode.com/problems/find-customer-referee/
+
+CREATE TABLE Customer (
+id number(4),
+name varchar2(20),
+referee_id number(4)
+);
+
+INSERT INTO CUSTOMER VALUES (1,'Will',null);
+INSERT INTO CUSTOMER VALUES (2,'Jane',null);
+INSERT INTO CUSTOMER VALUES (3,'Alex',2);
+INSERT INTO CUSTOMER VALUES (4,'Bill',null);
+INSERT INTO CUSTOMER VALUES (5,'Zack',1);
+INSERT INTO CUSTOMER VALUES (6,'Mark',2);
+
+
+
+
+
+*/
---
+++ TotalSalesByPdt_hrd.sql
@@ -0,0 +1,32 @@
+/*
+
+1384. Total Sales Amount by Year
+https://leetcode.com/problems/total-sales-amount-by-year/
+
+
+Create table product (
+product_id number(3),
+product_name varchar2(20)
+);
+
+INSERT INTO PRODUCT VALUES (1,'LC Phone');
+INSERT INTO PRODUCT VALUES (2,'LC T-Shirt');
+INSERT INTO PRODUCT VALUES (3,'LC Keychain');
+
+CREATE TABLE SALES (
+product_id number(3),
+period_start date,
+period_end date,
+average_daily_sales number(3)
+);
+
+INSERT INTO SALES VALUES (1,DATE'2019-01-25',DATE'2019-02-28',100);
+INSERT INTO SALES VALUES (2,DATE'2018-12-01',DATE'2020-01-01',10);
+INSERT INTO SALES VALUES (3,DATE'2019-12-01',DATE'2020-01-31',1);
+
+*/
+
+
+
+
+
